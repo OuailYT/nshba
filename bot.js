877838181432b866dae45d12073205792c3a5b09
OuailYT <42055439+OuailYT@users.noreply.github.com>
2019-02-08 18:24:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = "%";
+const prefix = "s!";
 console.log("Scrpit By OUAIL");
 
 
@@ -11,19 +11,30 @@ channel.send(`سبام كريدت`);
 }, 30)
 })
 
-client.on('message', msg => {
-    if (msg.content === 'كم معي') {
-      msg.reply('#credit');
-    }
-});
 
 client.on('message',function(message) {
-    let prefix = "%";
+    let prefix = "s!";
 let args = message.content.split(" ").slice(1).join(" ");
 if(message.content.startsWith(prefix + "حول")) {
 if(!args) return;
 message.channel.send(`#credit <@476577762396864512> ${args}`);
 }
+});
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+ 
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+ 
+  let args = message.content.split(" ").slice(1);
+ 
+//%say
+  if (command === "say") {
+          message.delete()
+    message.channel.sendMessage(args.join(" ")).catch(console.error);
+  }
 });
 
 client.login(process.env.BOT_TOKEN);
